5bb7c02d10e1a56adc160208be2d25e8168dbf9c
Ashot <macashot199514@gmail.com>
2019-03-04 21:56:34 +0400

    Initial Commit

---
+++ carouselApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  carouselApp
+//
+//  Created by Ashot on 3/4/19.
+//  Copyright © 2019 Ashot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
